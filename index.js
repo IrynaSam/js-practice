df95d7c3555eab1e44e65e1f66c9fd04ba089b7c
Maxim Samsonenko <samsonenko.max@gmail.com>
2021-10-19 21:29:41 +0300

    ok

---
+++ index.js
@@ -0,0 +1,13 @@
+// const userName = prompt("what is your name?");
+
+// if (userName.length < 4) {
+//     alert("This is way too short! Try again");
+//     userName;
+// } else {
+//     alert(`hello ${userName}`);
+// }
+
+while (userName.length < 4) {
+    userName;
+} 
+    alert(`hello ${userName}`);
